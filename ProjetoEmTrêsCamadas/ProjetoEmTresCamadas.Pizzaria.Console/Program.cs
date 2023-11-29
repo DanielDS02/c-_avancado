494fe46edddc22e6c38e184aebbe3c94f181d549
Daniel <danieldoleski02@gmail.comm>
2023-11-28 21:37:34 -0300

    início de projeto

---
+++ ProjetoEmTrêsCamadas/ProjetoEmTresCamadas.Pizzaria.Console/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Bem vindo a nossa pizzaria");
+Console.WriteLine("Gostaria de uma pizza, S para sim e N para não");
+var resposta = Console.ReadLine();
+Console.WriteLine(resposta);
+
+Console.WriteLine("Fim");
---
+++ ProjetoEmTrêsCamadas/ProjetoEmTresCamadas.Pizzaria.RegraDeNegocio/Pizza.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoEmTresCamadas.Pizzaria.RegraDeNegocio
+{
+    public enum TamanhoDePizza
+    {
+        Pequena = 0,
+        Média = 1,
+        Grande = 2,
+    }
+
+
+    public class Pizza
+    {
+        public string Sabor { get; set; }
+        public TamanhoDePizza TamanhoDePizza { get; set; }
+        public string Descricao {  get; set; }
+
+        public Pizza()
+        {
+            
+        }
+        public Pizza CriarPizza(string sabor, TamanhoDePizza tamanhoDePizza, string descricao = "")
+        {
+            Sabor = sabor;
+            this.TamanhoDePizza = tamanhoDePizza;
+            if(string.IsNullOrEmpty(descricao))
+            {
+                Descricao = descricao;
+            }
+            return this;
+            
+        }
+
+    }
+}
